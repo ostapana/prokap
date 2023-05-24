6e06a822556cdbd264bb641b8e64732799412a5a
ostapana <ostapana@fel.cvut.cz>
2023-05-24 22:43:53 +0200

    fix

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from PIL import Image
 
 from utils import *
 
-img_cv = cv2.imread(r'imgs/kobel.png')
-img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+# gray_img = cv2.imread(r'imgs/img.png', cv2.IMREAD_GRAYSCALE)
+gray_img = cv2.imread(r'imgs/good_kobel.png', cv2.IMREAD_GRAYSCALE)
+kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+# gray_img = apply_opening(gray_img, kernel_dil=kernel)
+gray_img = cv2.erode(gray_img, kernel, iterations=1)
+# resized_img = ResizeWithAspectRatio(gray_img, width=400)
+resized_img = ResizeWithAspectRatio(gray_img, width=900)
+print(pytesseract.image_to_string(gray_img))
 
-# resized_img = ResizeWithAspectRatio(img_rgb, width=1180)
-# cv2.imshow('output', resized_img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-print(pytesseract.image_to_string(img_rgb, config=r'--oem 3 --psm 6'))
-#
-# img_rgb = Image.frombytes('RGB', img_cv.shape[:2], img_cv, 'raw', 'BGR', 0, 0)
-# print(pytesseract.image_to_string(img_rgb))
+cv2.imshow('output', resized_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
     dim = None
@@ -14,3 +15,16 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
         dim = (width, int(h * r))
 
     return cv2.resize(image, dim, interpolation=inter)
+
+
+def apply_opening(img: np.ndarray, kernel_dil=None, kernel_erode=None, show=False):
+    """
+    Erosion followed by dilation
+    """
+    if kernel_dil is None:
+        kernel_dil = np.ones((3, 3), np.uint8)
+    if kernel_erode is None:
+        kernel_erode = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+    dilated_img = cv2.dilate(img, kernel_dil, iterations=1)
+    opened_img = cv2.erode(dilated_img, kernel_erode, iterations=1)
+    return opened_img
