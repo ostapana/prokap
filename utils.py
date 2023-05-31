5febe546e6972936d8e8c2aaa6e9ed04a82b67c8
ostapana <ostapana@fel.cvut.cz>
2023-05-31 22:37:48 +0200

    change imgs

--- utils.py
+++ utils.py
@@ -22,23 +22,6 @@ def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
     return cv2.resize(image, dim, interpolation=inter)
 
 
-def apply_closing(img: np.ndarray, kernel_dil=None, kernel_erode=None, show=False):
-    """
-    Dilation followed by erosion
-    """
-    if kernel_erode is None:
-        kernel_erode = np.ones((3, 3), np.uint8)
-    if kernel_dil is None:
-        kernel_dil = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-    dilated_img = cv2.dilate(img, kernel_dil, iterations=2)
-    closed_img = cv2.erode(dilated_img, kernel_erode, iterations=1)
-    dilated_img = cv2.dilate(closed_img, kernel_dil, iterations=1)
-    closed_img = cv2.erode(dilated_img, kernel_erode, iterations=2)
-    dilated_img = cv2.dilate(closed_img, kernel_dil, iterations=2)
-    closed_img = cv2.erode(dilated_img, kernel_erode, iterations=2)
-    return closed_img
-
-
 def apply_iterative_closing(img: np.ndarray, kernel_dil=None, kernel_erode=None):
     for i in range(10):
         img = cv2.dilate(img, kernel_dil, iterations=1)
@@ -57,3 +40,21 @@ def remove_black_background(img: np.ndarray) -> np.ndarray:
 def calculate_levenshtein_distance(str1, str2):
     distance = Levenshtein.distance(str1, str2)
     return distance
+
+
+class NegDatasetCreator():
+    def __init__(self, path_to_dataset):
+        self.path_to_init_dataset = path_to_dataset
+
+    def blur_image(self, img):
+        return cv2.blur(img, (5, 5))
+
+    def trim_image(self, img):
+        new_img = np.ndarray(shape=(0, img.shape[1]), dtype=np.uint8)
+        for i in range(0, img.shape[0]):
+            if i >= img.shape[0] * 0.66:  # 2/3 of the image size
+                break
+            row = img[i]
+            if not (row == 0).all():
+                new_img = np.concatenate((new_img, row[np.newaxis, ...]))
+        return new_img
